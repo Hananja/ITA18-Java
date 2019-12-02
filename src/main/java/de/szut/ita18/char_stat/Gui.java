1e37fcb74eeb85667d6552581865006af6a77a4b
Elmar Loos <e.loos@schule.bremen.de>
2019-12-02 22:10:45 +0100

    initial import

---
+++ src/main/java/de/szut/ita18/char_stat/Gui.java
@@ -0,0 +1,27 @@
+package de.szut.ita18.char_stat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Gui extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getClassLoader().getResource("char_stat.fxml"),
+                "FXML resource not found"));
+        primaryStage.setTitle("TextStat");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
